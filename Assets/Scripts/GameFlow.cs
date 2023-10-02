64235f6406317a106edbd0f2010fda19afe760e9
Alex Wang <54587384+Alextianyouwang@users.noreply.github.com>
2023-10-02 02:22:33 -0700

    MultipleTintAndGroup_10/02/2023

--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -12,23 +12,49 @@ public class GameFlow : MonoBehaviour
     public GameObject[] BlurCard_GroupTwo;
     public GameObject[] BlurCard_GroupThree;
     public GameObject[] BlurCard_GroupFour;
+    public GameObject[] BlurCard_Room3_TrashCan;
+    public GameObject[] BlurCard_Room3_Receipt;
+    public GameObject[] BlurCard_Room3_HDD;
     public PlayerInteract PlayerInteract;
+    public Color BlurCardTint;
     private float _timer;
 
     void Start()
     {
         ToggleBlurCard(BlurCard_GroupOne, true);
         SetBlurCardWeight(BlurCard_GroupOne, 0.2f);
+        SetBlurCardTint(BlurCard_GroupOne, Color.white);
 
         ToggleBlurCard(BlurCard_GroupTwo, true);
         SetBlurCardWeight(BlurCard_GroupTwo, 0.2f);
+        SetBlurCardTint(BlurCard_GroupTwo, Color.white);
+
 
         ToggleBlurCard(BlurCard_GroupThree, true);
         SetBlurCardWeight(BlurCard_GroupThree, 0.2f);
+        SetBlurCardTint(BlurCard_GroupThree, Color.white);
+
 
 
         ToggleBlurCard(BlurCard_GroupFour, true);
         SetBlurCardWeight(BlurCard_GroupFour, 0.2f);
+        SetBlurCardTint(BlurCard_GroupFour, Color.white);
+
+
+        ToggleBlurCard(BlurCard_Room3_TrashCan, true);
+        SetBlurCardWeight(BlurCard_Room3_TrashCan, 0.2f);
+        SetBlurCardTint(BlurCard_Room3_TrashCan, Color.white);
+
+
+        ToggleBlurCard(BlurCard_Room3_Receipt, true);
+        SetBlurCardWeight(BlurCard_Room3_Receipt, 0.2f);
+        SetBlurCardTint(BlurCard_Room3_Receipt, Color.white);
+
+
+        ToggleBlurCard(BlurCard_Room3_HDD, true);
+        SetBlurCardWeight(BlurCard_Room3_HDD, 0.2f);
+        SetBlurCardTint(BlurCard_Room3_HDD, Color.white);
+
 
 
 
@@ -46,6 +72,16 @@ public class GameFlow : MonoBehaviour
         {
             MaterialPropertyBlock mpb = new MaterialPropertyBlock();
             mpb.SetFloat("_Weight", target);
+            g.GetComponent<Renderer>().SetPropertyBlock(mpb);
+        }
+    }
+
+    void SetBlurCardTint(GameObject[] group, Color col)
+    {
+        foreach (GameObject g in group)
+        {
+            MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+            mpb.SetColor("_Color", col);
             g.GetComponent<Renderer>().SetPropertyBlock(mpb);
         }
     }
@@ -77,6 +113,12 @@ public class GameFlow : MonoBehaviour
     void DisableRoom3_BlurCard() 
     {
         ToggleBlurCard(BlurCard_GroupOne, false);
+
+        SetBlurCardTint(BlurCard_Room3_TrashCan, BlurCardTint);
+
+        SetBlurCardTint(BlurCard_Room3_Receipt, BlurCardTint);
+
+        SetBlurCardTint(BlurCard_Room3_HDD, BlurCardTint);
     }
 
     void SetTimer() 
